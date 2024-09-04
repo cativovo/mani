41f4ed04b7fd8771cc8b4867d2309049895bc1d9
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-04 16:10:50 +0800

    add shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import logo from "./assets/images/logo-universal.png";
-import "./App.css";
 import { Greet } from "@wails/go/main/App";
+import { useState } from "react";
+import "./App.css";
+import { Button } from "@/components/shadcn/button";
 
 function App() {
   const [resultText, setResultText] = useState(
@@ -16,23 +16,16 @@ function App() {
   }
 
   return (
-    <div id="App">
-      <img src={logo} id="logo" alt="logo" />
-      <div id="result" className="result">
-        {resultText}
-      </div>
-      <div id="input" className="input-box">
+    <div>
+      <div className="text-red-600">{resultText}</div>
+      <div>
         <input
-          id="name"
-          className="input"
           onChange={updateName}
           autoComplete="off"
           name="input"
           type="text"
         />
-        <button className="btn" onClick={greet}>
-          Greet
-        </button>
+        <Button onClick={greet}>Greet</Button>
       </div>
     </div>
   );
