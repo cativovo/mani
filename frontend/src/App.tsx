89d1745661668468dfeac673090e254733a75700
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-05 19:14:44 +0800

    allow copying of jq result

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { Editor } from "@monaco-editor/react";
 import { Query } from "@wails/go/main/App";
 import { ChangeEvent, useState } from "react";
 import "./App.css";
+import CopyButton from "@/components/CopyButton";
+import { Toaster } from "@/components/shadcn/sonner";
 
 const editorOptions = {
   minimap: { enabled: false },
@@ -59,42 +61,49 @@ function App() {
   const debouncedHandleFilterChange = debounce(handleFilterChange);
 
   return (
-    <div className="h-screen">
-      <div className="h-14 bg-red-600">
-        <label htmlFor="upload">
-          <span>upload file</span>
-          <input
-            type="file"
-            id="upload"
-            accept="json"
-            onChange={handleUpload}
-          />
-        </label>
-      </div>
-      <div className="flex h-[calc(100%-theme(space.14))] py-2">
-        <div className="w-[50vw] px-2">
-          <Editor
-            defaultLanguage="json"
-            value={json}
-            onChange={handleEditorChange}
-            onValidate={console.log}
-            options={editorOptions}
-            className="border py-2"
-          />
+    <>
+      <div className="h-screen">
+        <div className="h-14 bg-red-600">
+          <label htmlFor="upload">
+            <span>upload file</span>
+            <input
+              type="file"
+              id="upload"
+              accept="json"
+              onChange={handleUpload}
+            />
+          </label>
         </div>
-        <div className="w-[50vw] h-full px-2 flex flex-col gap-2">
-          <Textarea
-            className="resize-none h-24"
-            placeholder="your query here"
-            onChange={debouncedHandleFilterChange}
-          />
-          <ScrollArea className="w-full h-[calc(100%-theme(space.24))] px-2 py-4 border">
-            <JSONHighlighter json={jqResult} />
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
+        <div className="flex h-[calc(100%-theme(space.14))] py-2">
+          <div className="w-[50vw] px-2">
+            <Editor
+              defaultLanguage="json"
+              value={json}
+              onChange={handleEditorChange}
+              onValidate={console.log}
+              options={editorOptions}
+              className="border py-2"
+            />
+          </div>
+          <div className="w-[50vw] h-full px-2 flex flex-col gap-2">
+            <Textarea
+              className="resize-none h-24"
+              placeholder="your query here"
+              onChange={debouncedHandleFilterChange}
+            />
+            <ScrollArea className="w-full h-[calc(100%-theme(space.24))] px-2 py-4 border relative group">
+              <CopyButton
+                toCopy={jqResult}
+                className="absolute invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity top-2 right-4"
+              />
+              <JSONHighlighter json={jqResult} />
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </div>
         </div>
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
 
