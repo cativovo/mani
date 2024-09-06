81e1e1fd4b42ec5a7a648358314c0c175f86cc85
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-06 20:48:24 +0800

    add snapshot testing

--- go.mod
+++ go.mod
@@ -5,29 +5,40 @@ go 1.21
 toolchain go1.23.0
 
 require (
+	github.com/gkampitakis/go-snaps v0.5.7
 	github.com/itchyny/gojq v0.12.16
 	github.com/wailsapp/wails/v2 v2.9.1
 )
 
 require (
 	github.com/bep/debounce v1.2.1 // indirect
+	github.com/gkampitakis/ciinfo v0.3.0 // indirect
+	github.com/gkampitakis/go-diff v1.3.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/itchyny/timefmt-go v0.1.6 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/echo/v4 v4.10.2 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leaanthony/go-ansi-parser v1.6.0 // indirect
 	github.com/leaanthony/gosod v1.0.3 // indirect
 	github.com/leaanthony/slicer v1.6.0 // indirect
 	github.com/leaanthony/u v1.1.0 // indirect
+	github.com/maruel/natural v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/samber/lo v1.38.1 // indirect
+	github.com/tidwall/gjson v1.17.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
 	github.com/tkrajina/go-reflector v0.5.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
--- jq_test.go
+++ jq_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+)
 
 func TestRunQuery(t *testing.T) {
 	keyValueInput := `{"id":1,"name":"Bulbasaur","type":["Grass","Poison"],"abilities":[{"name":"Overgrow","description":"Powers up Grass-type moves when the Pokémon's HP is low."},{"name":"Chlorophyll","description":"Boosts the Pokémon's Speed stat in sunny weather.","hidden":true}],"stats":{"hp":45,"attack":49,"defense":49,"special_attack":65,"special_defense":65,"speed":45},"evolution":{"level":16,"evolves_to":{"id":2,"name":"Ivysaur"}},"moves":[{"name":"Tackle","type":"Normal","power":40,"accuracy":100,"pp":35},{"name":"Vine Whip","type":"Grass","power":45,"accuracy":100,"pp":25},{"name":"Leech Seed","type":"Grass","power":null,"accuracy":90,"pp":10},{"name":"Growl","type":"Normal","power":null,"accuracy":100,"pp":40}],"description":"Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."}`
@@ -10,34 +14,28 @@ func TestRunQuery(t *testing.T) {
 		name   string
 		input  string
 		filter string
-		want   string
 	}{
 		{
 			name:   "valid filter",
 			input:  keyValueInput,
 			filter: ".name",
-			want:   `"Bulbasaur"`,
 		},
 		{
 			name:   "invalid filter",
 			input:  keyValueInput,
 			filter: "jkjkjkjkjkjk",
-			want:   "function not defined: jkjkjkjkjkjk/0",
 		},
 		{
 			name:   "array input",
 			input:  arrayInput,
 			filter: ".",
-			want:   arrayInput,
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			got := runQuery(testCase.input, testCase.filter)
-			if got != testCase.want {
-				t.Errorf("got %q, want %q", got, testCase.want)
-			}
+			result := runQuery(testCase.input, testCase.filter)
+			snaps.MatchSnapshot(t, result)
 		})
 	}
 }
