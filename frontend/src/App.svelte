a678b06ee291d5dde2d761d64c67bb7b844a8fd8
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-15 15:49:10 +0800

    add shadcn-svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import Editor from "./components/Editor.svelte";
+	import Editor from "$/components/Editor.svelte";
+	import { Button } from "$/components/ui/button";
 </script>
 
 <main class="h-screen w-screen">
+	<Button>click me</Button>
 	<Editor
 		value={JSON.stringify({ foo: true }, null, 2)}
 		on:change={(e) => console.log(e.detail)}
