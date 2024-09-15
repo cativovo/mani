1958ca7bd252e4c86f0cbc94d9f3b50e0e1f2961
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-16 00:12:00 +0800

    add JSONHighlighter component

---
+++ frontend/src/components/JSONHighlighter.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import hljs from "highlight.js/lib/core";
+	import _json from "highlight.js/lib/languages/json";
+	import "highlight.js/styles/vs.min.css";
+	hljs.registerLanguage("json", _json);
+
+	export let json: string = "";
+</script>
+
+<pre class="p-2">
+<code>
+{@html hljs.highlight(json, { language: "json" }).value}
+</code>
+</pre>
