a13025d976f589905685ac1a0218dd3ae3db3e0f
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-15 15:53:14 +0800

    create JQPlayground component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-	import Editor from "$/components/Editor.svelte";
-	import { Button } from "$/components/ui/button";
+	import JqPlayground from "$/components/JQPlayground.svelte";
 </script>
 
-<main class="h-screen w-screen">
-	<Button>click me</Button>
-	<Editor
-		value={JSON.stringify({ foo: true }, null, 2)}
-		on:change={(e) => console.log(e.detail)}
-		on:validate={(e) => console.log(e.detail)}
-	/>
-</main>
+<JqPlayground />
---
+++ frontend/src/components/JQPlayground.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Editor from "./Editor.svelte";
+	import { Button } from "./ui/button";
+</script>
+
+<main class="h-screen w-screen">
+	<Button>click me</Button>
+	<Editor
+		value={JSON.stringify({ foo: true }, null, 2)}
+		on:change={(e) => console.log(e.detail)}
+		on:validate={(e) => console.log(e.detail)}
+	/>
+</main>
