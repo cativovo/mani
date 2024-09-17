a0b9b9fafe801c4459470d18ee6521e3e3b6f269
Mark Leo Cativo <cativomarkleo@gmail.com>
2024-09-17 18:40:54 +0800

    create shiki highlighter globally

--- frontend/src/components/Editor.svelte
+++ frontend/src/components/Editor.svelte
@@ -1,28 +1,11 @@
 <script lang="ts">
-	import { shikiToMonaco } from "@shikijs/monaco";
 	import * as monaco from "monaco-editor";
-	import { createHighlighter } from "shiki";
 	import { createEventDispatcher, onMount } from "svelte";
 
 	export let value: string = "";
 	export function setValue(value: string) {
 		editor?.setValue(value);
 	}
-
-	(async () => {
-		const languages = ["json"];
-
-		const highlighter = await createHighlighter({
-			themes: ["github-light-default"],
-			langs: languages,
-		});
-
-		languages.forEach((lang) => {
-			monaco.languages.register({ id: lang });
-		});
-
-		shikiToMonaco(highlighter, monaco);
-	})();
 
 	let editorElement: HTMLDivElement;
 	let editor: monaco.editor.IStandaloneCodeEditor;
--- frontend/src/monaco-worker.ts
+++ frontend/src/monaco-worker.ts
@@ -1,5 +1,8 @@
+import { shikiToMonaco } from "@shikijs/monaco";
+import * as monaco from "monaco-editor";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
 import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import { createHighlighter } from "shiki";
 
 self.MonacoEnvironment = {
   getWorker: function (_: any, label: string) {
@@ -9,3 +12,18 @@ self.MonacoEnvironment = {
     return new editorWorker();
   },
 };
+
+(async () => {
+  const languages = ["json"];
+
+  const highlighter = await createHighlighter({
+    themes: ["github-light-default"],
+    langs: languages,
+  });
+
+  languages.forEach((lang) => {
+    monaco.languages.register({ id: lang });
+  });
+
+  shikiToMonaco(highlighter, monaco);
+})();
